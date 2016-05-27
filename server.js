ce0782a04b9866b85240005dbfb59f6347ace7cf
Luciano Junior <luciannojunior@gmail.com>
2016-05-27 11:48:24 -0300

    oi

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 #!/bin/env node
+//  OpenShift sample Node application
 var express = require('express');
 var app = express();
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-app.get('/', function (req, res) {
-    res,json({
-        "no ar": "sim"
+app.get('/lu', function (req, res) {
+    res.writeHead(302, {
+    'Location': 'http://ludelutriz-luciannojunior.rhcloud.com/'
     });
+    res.end();
 });
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP, function () {
-    console.log("estamos no ar");
+app.get('/', function (req, res) {
+    res.writeHead(302, {
+    'Location': 'http://facebook.com/lutrizfotos'
+    });
+    res.end();
+});
+app.listen(port, ipaddress, function () {
+    console.log('Server is on, bitches!');
 });
