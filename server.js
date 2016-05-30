eda81977b79051996f8f3b2addfd490087dbae9c
Luciano Junior <luciannojunior@gmail.com>
2016-05-30 17:56:53 -0300

    script de request

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
-#!/bin/env node
-var express = require('express');
 var request = require('request');
-var app = express();
-
-app.get('/', function (req, res) {
-    request({
-    url: 'https://pre.ufcg.edu.br:8443/ControleAcademicoOnline/Controlador', //URL to hit
-    method: 'POST',
-    //Lets post the following key/values as form
-    form: { login: '115110125', senha: 'nicolas9', command: 'AlunoLogin' }
-}, function(error, response, body){
-    res.send(error);
-    res.send(body);
-    res.send(response);
-});
+// isso que faz ele nao rejeitar uma requisicao com erro de certificado
+var request = request.defaults({
+  strictSSL: false,
+  rejectUnauthorized: false
 });
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP, function () {
-    console.log("estamos no ar");
+var prompt = require('prompt');
+// libzinha para ler dados do console
+prompt.start();
+
+  var properties = [
+    {
+      name: 'matricula'
+    },
+    {
+      name: 'senha',
+      hidden: true
+    }
+  ];
+  
+prompt.get(properties, function (err, result) {
+  if (err){
+    console.log("Deu merda: "+error);
+  }
+  var options = { method: 'POST',
+                url: 'https://pre.ufcg.edu.br:8443/ControleAcademicoOnline/Controlador',
+                headers: 
+                { 'content-type': 'application/x-www-form-urlencoded',
+                  'postman-token': '42a2ce0c-350e-630f-1956-54f2da81e305',
+                  'cache-control': 'no-cache' },
+                  form: { login: result.matricula  , senha: result.senha, command: 'AlunoLogin' },
+              };
+  request(options, function (error, response, body) {
+    if (error){
+        console.log("Deu merda: "+error);
+    }
+    console.log(body);
+  });
 });
