3e69487f6c6ca562cde784571d2b84efd8b1b974
Luciano Junior <luciannojunior@gmail.com>
2016-05-31 11:35:53 -0300

    html2json

--- server.js
+++ server.js
@@ -1,40 +1,76 @@
 var request = require('request');
+var himalaya = require('himalaya');
+var htmlStr = require('html-strings');
+var fs = require('fs');
+var escape = htmlStr.escape;
+const URL_CONTROLE = 'https://pre.ufcg.edu.br:8443/ControleAcademicoOnline/Controlador';
+
 // isso que faz ele nao rejeitar uma requisicao com erro de certificado
 var request = request.defaults({
   strictSSL: false,
   rejectUnauthorized: false
 });
 
+var controle = {};
+/**
+ * Realiza requisicoes de pegar as paginas do ControleAcademicoOnline
+ * Deve ser chamada apos o login ter ocorrido
+ */
+controle.get = function (comando, cb) {
+  var config = {
+      method: 'GET',
+      url: URL_CONTROLE + '?command=' + comando,
+      jar: true
+  };
+  request(config, function (erro, resposta, corpo) {
+    if (erro){
+      console.log("ERRO: "+erro);
+    }
+    return cb(corpo);
+  });
+};
+/**
+ * Realiza requisicao Inicial de Login
+ */
+controle.login = function (aluno, cb) {
+    var config = {
+        method: 'POST',
+        url: URL_CONTROLE,
+        form: {
+          login: aluno.login,
+          senha: aluno.senha,
+          command: 'AlunoLogin'
+        },
+        jar: true
+    };
+    request(config, function (erro, resposta, corpo) {
+        if (erro){
+          console.log(erro);
+        }
+        cb(corpo);
+    });
+};
+
 var prompt = require('prompt');
 // libzinha para ler dados do console
 prompt.start();
 
-  var properties = [
-    {
-      name: 'matricula'
-    },
-    {
-      name: 'senha',
-      hidden: true
-    }
-  ];
-  
+var properties = [
+  { name: 'matricula' },
+  { name: 'senha', hidden: true }
+];
+
 prompt.get(properties, function (err, result) {
   if (err){
     console.log("Deu merda: "+error);
   }
-  var options = { method: 'POST',
-                url: 'https://pre.ufcg.edu.br:8443/ControleAcademicoOnline/Controlador',
-                headers: 
-                { 'content-type': 'application/x-www-form-urlencoded',
-                  'postman-token': '42a2ce0c-350e-630f-1956-54f2da81e305',
-                  'cache-control': 'no-cache' },
-                  form: { login: result.matricula  , senha: result.senha, command: 'AlunoLogin' },
-              };
-  request(options, function (error, response, body) {
-    if (error){
-        console.log("Deu merda: "+error);
-    }
-    console.log(body);
+  controle.login({login: result.matricula, senha: result.senha}, function (corpo) {
+    controle.get('AlunoTurmasListar', function (corpo) {
+      var json = himalaya.parse(corpo);
+      fs.writeFile('dump.json', JSON.stringify(json));
+    });
+    
   });
+  
+  
 });
