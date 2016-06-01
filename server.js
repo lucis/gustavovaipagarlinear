afac8f5d4f5f98ad2b61ac94dc40715871c808b2
Estácio <estacio.neto@ccc.ufcg.edu.br>
2016-06-01 20:49:53 -0300

    utf-8

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var request = require("request"), iconv  = require('iconv-lite');
 var himalaya = require('himalaya');
 var htmlStr = require('html-strings');
 var utf8 = require('utf8');
@@ -20,16 +20,19 @@ var controle = {};
  */
 controle.get = function (comando, cb) {
   var config = {
-      method: 'GET',
-      url: URL_CONTROLE + '?command=' + comando,
-      "Accept-Charset": "utf-8",
-      jar: true
+    encoding : null,
+    method: 'GET',
+    url: URL_CONTROLE + '?command=' + comando,
+    "Accept-Charset": "utf-8",
+    jar: true
   };
   request(config, function (erro, resposta, corpo) {
-    if (erro){
-      console.log("ERRO: "+erro);
+    // utf-8
+    corpo = iconv.decode(new Buffer(corpo), "ISO-8859-1");
+    if (erro) {
+      console.log("ERRO: " + erro);
     }
-    console.log(utf8.encode(corpo))
+    console.log(corpo)
     return cb(corpo);
   });
 };
@@ -37,22 +40,22 @@ controle.get = function (comando, cb) {
  * Realiza requisicao Inicial de Login
  */
 controle.login = function (aluno, cb) {
-    var config = {
-        method: 'POST',
-        url: URL_CONTROLE,
-        form: {
-          login: aluno.login,
-          senha: aluno.senha,
-          command: 'AlunoLogin'
-        },
-        jar: true
-    };
-    request(config, function (erro, resposta, corpo) {
-        if (erro){
-          console.log(erro);
-        }
-        cb(corpo);
-    });
+  var config = {
+    method: 'POST',
+    url: URL_CONTROLE,
+    form: {
+      login: aluno.login,
+      senha: aluno.senha,
+      command: 'AlunoLogin'
+    },
+    jar: true
+  };
+  request(config, function (erro, resposta, corpo) {
+    if (erro) {
+      console.log(erro);
+    }
+    cb(corpo);
+  });
 };
 
 var prompt = require('prompt');
@@ -71,8 +74,8 @@ var properties = [
 //   controle.login({login: result.matricula, senha: result.senha}, function (corpo) {
 //     controle.get('AlunoTurmasListar', function (corpo) {
 //       var json = himalaya.parse(corpo);
-      
-      
+
+
 //       function search(path, obj, target) {
 //           for (var k in obj) {
 //               if (obj.hasOwnProperty(k))
@@ -102,51 +105,51 @@ var properties = [
 //       }
 //       // fs.writeFile('dump.json', JSON.stringify(json));
 //     });
-    
-//   });
-  
-  
-// });
-controle.login({login: '115110125', senha: 'nicolas9'}, function (corpo) {
-    controle.get('AlunoTurmasListar', function (corpo) {
-      var json = himalaya.parse(utf8.encode(corpo));
-      function search(path, obj, target) {
-          for (var k in obj) {
-              if (obj.hasOwnProperty(k))
-                  if (obj[k] === target)
-                      return path + "['" + k + "']"
-                  else if (typeof obj[k] === "object") {
-                      var result = search(path + "['" + k + "']", obj[k], target);
-                      if (result)
-                          return result;
-                  }
-          }
-          return false;
-      }
 
-      var path = search("json", json, "04");
-      // console.log(path);
-      // CALCULO
-      //json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children']['1']['children']['5']['children']['1']['children']['0']['content']
-      // LP2
-      //json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children']['3']['children']['5']['children']['1']['children']['0']['content']
-      // DISCRETA
-      // json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children']['5']['children']['5']['children']['1']['children']['0']['content']
-      // console.log(path); //data['key1']['children']['key4']
-      
-      var disciplinas = [];
-      for (var i = 1; i < json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'].length; i = i + 2){
-        var disciplina = {};
-        disciplina.nome = json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['5']['children']['1']['children']['0']['content'];
-        disciplina.url = json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['5']['children']['1']['attributes']['href'];
-        disciplina.horarios = "";
-        for (var j = 0; j < json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['9']['children'].length; j = j + 2){
-          disciplina.horarios += json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['9']['children'][j]['content'];
-        }
-        disciplina.turma = json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['7']['children']['0']['content'];
-        // console.log(disciplina);
+//   });
+
+
+// });
+controle.login({ login: '115110125', senha: 'nicolas9' }, function (corpo) {
+  controle.get('AlunoTurmasListar', function (corpo) {
+    var json = himalaya.parse(corpo);
+    function search(path, obj, target) {
+      for (var k in obj) {
+        if (obj.hasOwnProperty(k))
+          if (obj[k] === target)
+            return path + "['" + k + "']"
+          else if (typeof obj[k] === "object") {
+            var result = search(path + "['" + k + "']", obj[k], target);
+            if (result)
+              return result;
+          }
       }
-      fs.writeFile('dump.json', JSON.stringify(json));
-    });
-    
+      return false;
+    }
+
+    var path = search("json", json, "04");
+    // console.log(path);
+    // CALCULO
+    //json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children']['1']['children']['5']['children']['1']['children']['0']['content']
+    // LP2
+    //json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children']['3']['children']['5']['children']['1']['children']['0']['content']
+    // DISCRETA
+    // json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children']['5']['children']['5']['children']['1']['children']['0']['content']
+    // console.log(path); //data['key1']['children']['key4']
+
+    var disciplinas = [];
+    for (var i = 1; i < json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'].length; i = i + 2) {
+      var disciplina = {};
+      disciplina.nome = json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['5']['children']['1']['children']['0']['content'];
+      disciplina.url = json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['5']['children']['1']['attributes']['href'];
+      disciplina.horarios = "";
+      for (var j = 0; j < json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['9']['children'].length; j = j + 2) {
+        disciplina.horarios += json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['9']['children'][j]['content'];
+      }
+      disciplina.turma = json['2']['children']['3']['children']['6']['children']['3']['children']['3']['children'][i]['children']['7']['children']['0']['content'];
+      // console.log(disciplina);
+    }
+    fs.writeFile('dump.json', JSON.stringify(json));
   });
+
+});
